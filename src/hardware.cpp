864177494ddfa772d819216ae2a405bd60da462d
Will Fox <will@foxieproducts.com>
2024-04-20 18:32:58 -0500

    Add hardware test for FC2

--- src/hardware.cpp
+++ src/hardware.cpp
@@ -1,10 +1,15 @@
 #include <hardware.hpp>
 #include <light_sensor.hpp>
 
-static void HardwareTest_CC2(std::shared_ptr<Pixels> pixels,
-                             std::shared_ptr<Settings> settings,
-                             std::shared_ptr<Rtc> rtc,
-                             std::shared_ptr<Joystick> joy);
+static void HardwareTest_CardClock2(std::shared_ptr<Pixels> pixels,
+                                    std::shared_ptr<Settings> settings,
+                                    std::shared_ptr<Rtc> rtc,
+                                    std::shared_ptr<Joystick> joy);
+
+static void HardwareTest_FoxieClock2(std::shared_ptr<Pixels> pixels,
+                                     std::shared_ptr<Settings> settings,
+                                     std::shared_ptr<Rtc> rtc,
+                                     std::shared_ptr<Joystick> joy);
 
 void ShowSerialStatusMessage(std::shared_ptr<Pixels> pixels,
                              std::shared_ptr<Rtc> rtc) {
@@ -28,13 +33,17 @@ void DoHardwareStartupTests(std::shared_ptr<Pixels> pixels,
 #endif
 
 #if FCOS_FOXIECLOCK
+    // Because there are many FC2s in the field that already passed the hardware
+    // test, default it to passing for the FC2.
     (*settings)["TEST"] = 1;
 #endif
 
     if (joy->AreAnyButtonsPressed() == PIN_BTN_UP ||
         (*settings)["TEST"].as<int>() == 0) {
 #if FCOS_CARDCLOCK2
-        HardwareTest_CC2(pixels, settings, rtc, joy);
+        HardwareTest_CardClock2(pixels, settings, rtc, joy);
+#elif FCOS_FOXIECLOCK
+        HardwareTest_FoxieClock2(pixels, settings, rtc, joy);
 #endif
     } else if (joy->AreAnyButtonsPressed() == PIN_BTN_LEFT) {
         // clear settings and restart
@@ -51,10 +60,10 @@ void DoHardwareStartupTests(std::shared_ptr<Pixels> pixels,
     }
 }
 
-void HardwareTest_CC2(std::shared_ptr<Pixels> pixels,
-                      std::shared_ptr<Settings> settings,
-                      std::shared_ptr<Rtc> rtc,
-                      std::shared_ptr<Joystick> joy) {
+void HardwareTest_CardClock2(std::shared_ptr<Pixels> pixels,
+                             std::shared_ptr<Settings> settings,
+                             std::shared_ptr<Rtc> rtc,
+                             std::shared_ptr<Joystick> joy) {
     auto beginUptime = rtc->Uptime();
     pixels->DrawChar(0, 3, CHAR_UP_ARROW, ORANGE);
     pixels->Show();
@@ -91,6 +100,7 @@ void HardwareTest_CC2(std::shared_ptr<Pixels> pixels,
 
     pixels->Clear();
     pixels->Show();
+    ElapsedTime::Delay(50);
     LightSensor ls;
     ls.SetHwMax(LightSensor::HW_MAX);
 
@@ -121,6 +131,86 @@ void HardwareTest_CC2(std::shared_ptr<Pixels> pixels,
         pixels->Show();
     } else {
         pixels->DrawText(1, 3, " OK ", GREEN);  // everything and RTC is good
+        pixels->Show();
+        (*settings)["TEST"] = 1;  // test success
+        (*settings).Save();
+        joy->WaitForNoButtonsPressed();
+    }
+    ElapsedTime::Delay(1000);
+}
+
+void HardwareTest_FoxieClock2(std::shared_ptr<Pixels> pixels,
+                              std::shared_ptr<Settings> settings,
+                              std::shared_ptr<Rtc> rtc,
+                              std::shared_ptr<Joystick> joy) {
+    auto beginUptime = rtc->Uptime();
+    pixels->DrawChar(0, 3, CHAR_UP_ARROW, ORANGE);
+    pixels->Show();
+    joy->WaitForNoButtonsPressed();
+    joy->WaitForButton(joy->up);
+    pixels->DrawChar(0, 0, CHAR_UP_ARROW, GREEN);
+
+    pixels->DrawChar(0, 0, CHAR_DOWN_ARROW, ORANGE);
+    pixels->Show();
+    joy->WaitForButton(joy->down);
+    pixels->DrawChar(0, 0, CHAR_DOWN_ARROW, GREEN);
+
+    pixels->DrawChar(62, 0, '?', ORANGE);
+    pixels->Show();
+    joy->WaitForButton(joy->right);
+    pixels->DrawChar(62, 0, '?', GREEN);
+
+    pixels->DrawChar(0, 0, '?', ORANGE);
+    pixels->Show();
+    joy->WaitForButton(joy->left);
+    pixels->DrawChar(0, 0, '?', GREEN);
+
+    pixels->Clear();
+    pixels->DrawChar(20, 0, '?', ORANGE);
+    pixels->DrawChar(42, 0, '?', ORANGE);
+    pixels->Show();
+    joy->WaitForButton(joy->press);
+
+    joy->WaitForNoButtonsPressed();
+    for (int i = 0; i < TOTAL_ALL_LEDS; ++i) {
+        pixels->Set(i, Pixels::ColorWheel(i));
+    }
+    pixels->Show();
+    joy->WaitForButton(joy->press);
+
+    pixels->Clear();
+    pixels->Show();
+    ElapsedTime::Delay(50);
+    LightSensor ls;
+    ls.SetHwMax(LightSensor::HW_MAX);
+
+    bool found = false;
+    for (size_t i = 0; i <= 10; ++i) {
+        ls.SetHwMin(LightSensor::HW_MIN + i);
+        ls.ResetToCurrentSensorValue();
+        if (ls.GetScaled() < 0.001f) {
+            found = true;
+            DPRINT("Found min:%d\n", LightSensor::HW_MIN + i);
+            break;
+        }
+    }
+    if (!found) {
+        pixels->Clear();
+        pixels->DrawText(0, 0, "ERR1", PURPLE);
+        pixels->Show();
+        joy->WaitForButton(joy->press);
+    } else {
+        (*settings)["LS_HW_MIN"] = ls.GetHwMin();
+        (*settings)["LS_HW_MAX"] = ls.GetHwMax();
+        DPRINT("LS_HW_MIN: %d\n", ls.GetHwMin());
+    }
+
+    pixels->Clear();
+    if (rtc->Uptime() != beginUptime) {
+        pixels->DrawText(0, 0, "????", RED);
+        pixels->Show();
+    } else {
+        pixels->DrawText(0, 0, "11:11", GREEN);  // everything and RTC is good
         pixels->Show();
         (*settings)["TEST"] = 1;  // test success
         (*settings).Save();
